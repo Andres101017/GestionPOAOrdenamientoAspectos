c44c00dff98772d91759f2ccca0daebbcd0937db
Andres Combariza <andres.combariza@utp.edu.co>
2022-12-05 20:17:03 -0500

    Se agrega AOP V. Reutilización de pointcut expressions..

--- src/main/java/es/pildoras/aop/aspectos/LoginConAspecto.java
+++ src/main/java/es/pildoras/aop/aspectos/LoginConAspecto.java
@@ -2,18 +2,34 @@ package es.pildoras.aop.aspectos;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoginConAspecto {
 	
+	@Pointcut("execution(public * insertaCliente(..))")
+	private void paraClientes() {}
+	
+	
 	//@Before("execution(public void es.pildoras.aop.dao.ClienteVIPDAO.insertaCliente())")
 	//@Before("execution(public Void insertaCliente*())")
 	//@Before("execution(public String insertaCliente*())")
-	@Before("execution(public * insertaCliente*(..))")
+	@Before("paraClientes()")
 	public void antesInsertar() {
 		System.out.println("El usuario esta logueado.");
 		System.out.println("El perfil para insertar clientes es correcto.");
+	}
+	
+	@Before("paraClientes()")
+	public void requisitosCliente() {
+		System.out.println("El cliente cumple los requisitos para ser insertado en la base de datos.");
+	}
+	
+	@Before("paraClientes()")
+	public void requisitosTabla() {
+		System.out.println("Hay menos de 3000 registros, en al tabla, puedes insertar el nuevo cliente.");
+
 	}
 }
