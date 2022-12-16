5a2a0416aa3d5d1d9cdf5066b115f651017f2e36
Andres Combariza <andres.combariza@utp.edu.co>
2022-12-15 21:08:00 -0500

    Proyecto  para explicar la ordenación de Aspectos.

--- src/main/java/es/pildoras/aop/aspectos/LoginConAspecto.java
+++ src/main/java/es/pildoras/aop/aspectos/LoginConAspecto.java
@@ -3,46 +3,25 @@ package es.pildoras.aop.aspectos;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class LoginConAspecto {
 	
 	//@Pointcut("execution(public * insertaCliente(..))")
 	@Pointcut("execution(* es.pildoras.aop.dao.*.*(..))")
-	private void paraClientes() {}
-	
-	// Pointcut para Getters
-	@Pointcut("execution(* es.pildoras.aop.dao.*.get*(..))")
-	private void paraGetters() {}
-	
-	// Pointcut para Setters
-	@Pointcut("execution(* es.pildoras.aop.dao.*.set*(..))")
-	private void paraSetters() {}
-	
-	// Combinacion de Pointcut
-	@Pointcut("paraClientes() && !(paraGetters() || paraSetters())")
-	private void paqueteExceptoGetterSetter() {}
+	public void paraClientes() {}
 	
 	//@Before("execution(public void es.pildoras.aop.dao.ClienteVIPDAO.insertaCliente())")
 	//@Before("execution(public Void insertaCliente*())")
 	//@Before("execution(public String insertaCliente*())")
-	//@Before("paraClientes()")
-	@Before("paqueteExceptoGetterSetter()")
+	@Before("paraClientes()")
+	//@Before("paqueteExceptoGetterSetter()")
 	public void antesInsertarCliente() {
 		System.out.println("El usuario esta logueado.");
 		System.out.println("El perfil para insertar clientes es correcto.");
-	}
-	
-	//@Before("paraClientes()")
-	public void requisitosCliente() {
-		System.out.println("El cliente cumple los requisitos para ser insertado en la base de datos.");
-	}
-	
-	//@Before("paraClientes()")
-	public void requisitosTabla() {
-		System.out.println("Hay menos de 3000 registros, en al tabla, puedes insertar el nuevo cliente.");
-
 	}
 }
--- src/main/java/es/pildoras/aop/dao/ClienteDAO.java
+++ src/main/java/es/pildoras/aop/dao/ClienteDAO.java
@@ -9,26 +9,4 @@ public class ClienteDAO {
 	public void insertaCliente(Cliente cliente, String tipo) {
 		System.out.println("Trabajo realizado OK, CLiente insertado con éxito.");
 	}
-	
-	
-	public String getValoracionClienteNormal() {
-		System.out.println("Obteniendo valoración del Cliente.");
-		return valoracionClienteNormal;
-	}
-	public void setValoracionClienteNormal(String valoracionClienteNormal) {
-		System.out.println("Estableciendo valoración del Cliente.");
-		this.valoracionClienteNormal = valoracionClienteNormal;
-	}
-	public String getCodigoClienteNormal() {
-		System.out.println("Obteniendo Código del Cliente.");
-		return codigoClienteNormal;
-	}
-	public void setCodigoClienteNormal(String codigoClienteNormal) {
-		System.out.println("Estableciendo Código del Cliente.");
-		this.codigoClienteNormal = codigoClienteNormal;
-	}
-
-
-	private String valoracionClienteNormal;
-	private String codigoClienteNormal;
 }
